6efe5918ff9ac3268ef8a7cd9834ef7b13ef7189
Kronos Dev <dev@kronos-nse.local>
2026-06-04 05:53:56 +0000

    feat(03-03): register OICollector in collectors __init__.py

--- variance/collectors/__init__.py
+++ variance/collectors/__init__.py
@@ -5,5 +5,6 @@ from __future__ import annotations
 from variance.collectors.vix_collector import VIXCollector
 from variance.collectors.options_collector import OptionsCollector
 from variance.collectors.fii_dii_collector import FIIDIICollector
+from variance.collectors.oi_collector import OICollector
 
-__all__ = ["VIXCollector", "OptionsCollector", "FIIDIICollector"]
+__all__ = ["VIXCollector", "OptionsCollector", "FIIDIICollector", "OICollector"]
