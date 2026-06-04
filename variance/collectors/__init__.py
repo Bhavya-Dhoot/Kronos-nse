744927825aecbf5ab794f1153573d8eca968e7df
Kronos Dev <dev@kronos-nse.local>
2026-06-04 05:51:52 +0000

    feat(03-institutional-flow): register FIIDIICollector in __init__ exports

--- variance/collectors/__init__.py
+++ variance/collectors/__init__.py
@@ -3,5 +3,7 @@
 from __future__ import annotations
 
 from variance.collectors.vix_collector import VIXCollector
+from variance.collectors.options_collector import OptionsCollector
+from variance.collectors.fii_dii_collector import FIIDIICollector
 
-__all__ = ["VIXCollector"]
+__all__ = ["VIXCollector", "OptionsCollector", "FIIDIICollector"]
